5041c7dbe3a1e202470ef7f8ad43c71c25ae5f4e
ZiselSegal <zisel.segal@gmail.com>
2026-02-04 13:55:53 +0200

    added basic menu with manager password validation

--- funcs.py
+++ funcs.py
@@ -12,7 +12,7 @@ def login_management(user, password, retorn_of_credentials):
     for one_item in retorn_of_credentials:
         if one_item[0] == user and one_item[1] == password:
             return True
-        return False
+    return False
 
 # recieves nothing returns matrix of file contents where each row is a list inside the matrix
 def load_credentials():
@@ -20,7 +20,7 @@ def load_credentials():
         reader = csv.reader(f)
         return list(reader)[1::]
 
-
+#checks if all files needed for project are present and close the program if not return true or false
 def File_existence_check():
     files_to_check = ['airport_entry_fee.csv','available_flights.json','budget.txt','continents_pricing.csv','credentials.csv','funcs.py','main.py','menu.py']
     for file in files_to_check:
--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+import funcs as fun
+
+#handles user UI and input logic recieves nothing and returns None
 def menu():
     print('1.manager\n'
           '2.customer\n'
@@ -5,9 +8,16 @@ def menu():
     action = input('plz enter action number: ')
     match action:
         case '1':
-            name = input('plz enter user name: ')
-            password = input('plz enter password: ')
+            validation = fun.login_management(user='a',password=None,retorn_of_credentials=[])
+            while validation == False:
+                name = input('plz enter user name: ')
+                password = input('plz enter password: ')
+                credentials = fun.load_credentials()
+                validation = fun.login_management(name,password,credentials)
+                if validation == False:
+                    print('invalid login')
         case '2':
             pass
         case '3':
             exit()
+menu()
